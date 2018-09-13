744395d898600711ebe89ee48e2553ec3dfe9ffe
Raniere <raniereslourenco@gmail.com>
2018-09-13 17:20:46 -0300

    cursophp7

--- Dao/index.php
+++ Dao/index.php
@@ -2,10 +2,17 @@
 
 require_once ("config.php");
 
+$root = new Usuario();
+
+$root->loadById(4);
+
+echo $root;
+
+/*
 $sql = new Sql();
 
 $usuarios = $sql->select("SELECT * FROM TB_USUARIOS");
 
 echo json_encode($usuarios);
-
+*/
  ?>
